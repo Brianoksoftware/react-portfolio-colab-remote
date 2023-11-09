abfcb9a67d843b59da0f60c6070f1dd1d1ad33cf
Brianoksoftware <brianokdev@gmail.com>
2023-11-10 02:25:12 +0300

    centering About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,32 +2,21 @@ import React from 'react';
 
 export default function About() {
   return (
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-      
-      
-      
-      
-      
-      <div className="flex flex-wrap justify-center">
-      
-       {/*<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">*/}
-       <div className="lg:flex-grow lg:pr-8 md:w-1/2 w-full mb-16 md:mb-0">
+    <section id="about" className="py-20 bg-gray-900">
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="md:w-1/2 md:pr-8 flex-shrink-0 mb-16 md:mb-0">
           <img
-            className="object-cover object-center rounded"
+            className="object-cover object-center rounded w-full"
             alt="hero"
-            
             src="./space0.svg"
-            //src="./brianbrian.jpg"
           />
         </div>
-        
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center mt-32 pl-10">
+        <div className="md:w-1/2 md:pl-8 flex flex-col items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Brian
             <br className="" /> I am a Full-stack engineer.
           </h1>
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8 leading-relaxed text-gray-400">
             I am skilled in various languages, frameworks & tools.
             Including React, Python, Javascript, Mojo, Html, CSS, Tailwind CSS, Mongo DB,
             Express, Node.js, React Native, Git, Flask, Postgresql, Linux, Ionic, Data structures, Algorithms, heroku, Angular and Capacitor
@@ -35,32 +24,16 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+              className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg mr-4">
               Contact Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              className="inline-flex text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 rounded text-lg">
               Projects
             </a>
           </div>
         </div>
-        
-      </div>  
-      
-      
-      
-
-       {/*} <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            
-            src="./space0.svg"
-            //src="./brianbrian.jpg"
-
-          />
-        </div> */}
       </div>
     </section>
   );
