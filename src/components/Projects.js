35f00f088f0d892674dfd9b0b273bbbfaa11bb34
Brianoksoftware <brianokdev@gmail.com>
2023-11-10 12:55:47 +0300

    fixing Projects component svg images sides cut off on smaller screens

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <BriefcaseIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Projects.
+            Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             I have worked on several software development projects and will be updating my portfolio soon.
@@ -20,35 +20,32 @@ export default function Projects() {
         
         
         
-        <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-2 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-purple-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
+      <div className="flex flex-wrap -m-4">
+        {projects.map((project) => (
+          <a
+            href={project.link}
+            key={project.image}
+            className="sm:w-1/2 w-full p-4"> {/* Adjusted width for small screens so images take up whole width */}
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                style={{ objectFit: 'cover' }} // Added objectFit style inlinely so img scales maintaining  aspect ratio while fully covering content box
+                src={project.image}
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-2 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-purple-400 mb-1">
+                  {project.subtitle}
+                </h2>
+                <h1 className="title-font text-lg font-medium text-white mb-3">
+                  {project.title}
+                </h1>
+                <p className="leading-relaxed">{project.description}</p>
               </div>
-            </a>
-            
-          )
-            
-          )}
-        </div>
+            </div>
+          </a>
+        ))}
+      </div>
         
         
         
